b33845a5ea7f72a3fd0e1b0c66eb6f162af1ea75
minwonki <wk.min@braincommerce.com>
2019-02-27 17:33:05 +0900

    3 commit

--- app/src/main/java/com/wk/min/libtesting/MainActivity.kt
+++ app/src/main/java/com/wk/min/libtesting/MainActivity.kt
@@ -10,7 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val simple = SimpleClass()
         simple.showMessage("minwonki")
+        simple.showMessage2("minwonki")
     }
 }
--- dialoglib/src/main/java/com/wk/min/dialoglib/SimpleClass.java
+++ dialoglib/src/main/java/com/wk/min/dialoglib/SimpleClass.java
@@ -4,4 +4,8 @@ public class SimpleClass {
     public void showMessage(String message) {
         System.out.println("message:" + message);
     }
+
+    public void showMessage2(String message) {
+        System.out.println("message222:" + message);
+    }
 }
