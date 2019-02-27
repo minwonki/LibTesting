d2834aca8dabef282c0fc6bbfb1f2b648ced8ed8
minwonki <wk.min@braincommerce.com>
2019-02-27 17:07:17 +0900

    second commit

--- app/src/main/java/com/wk/min/libtesting/MainActivity.kt
+++ app/src/main/java/com/wk/min/libtesting/MainActivity.kt
@@ -2,11 +2,15 @@ package com.wk.min.libtesting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.wk.min.dialoglib.SimpleClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simple = SimpleClass()
+        simple.showMessage("minwonki")
     }
 }
---
+++ dialoglib/src/main/java/com/wk/min/dialoglib/SimpleClass.java
@@ -0,0 +1,7 @@
+package com.wk.min.dialoglib;
+
+public class SimpleClass {
+    public void showMessage(String message) {
+        System.out.println("message:" + message);
+    }
+}
